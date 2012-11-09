e4d033f1f00679b2317969f01b7d887c0623a6d5
c-aries <babyaries2@gmail.com>
2012-11-10 01:01:37 +0800

    update check im method

--- src/util.c
+++ src/util.c
@@ -27,25 +27,13 @@ enum im_type
 check_current_input_method ()
 {
   if (process_exist ("scim")) {
-#ifdef DEBUG
-    g_print ("%s\n", "scim");
-#endif
     return IM_SCIM;
   }
-  else if (process_exist ("ibus")) {
-#ifdef DEBUG
-    g_print ("%s\n", "ibus");
-#endif
+  else if (process_exist ("ibus-daemon")) {
     return IM_IBUS;
   }
   else if (process_exist ("fcitx")) {
-#ifdef DEBUG
-    g_print ("%s\n", "fcitx");
-#endif
     return IM_FCITX;
   }
-#ifdef DEBUG
-    g_print ("%s\n", "other");
-#endif
   return IM_OTHER;
 }
